b4927a2fb4be231393ac922561459ca5fb9ad634
Schlichtkrull <micschli@8c85903f7e66.ant.amazon.com>
2018-08-08 17:28:36 +0200

    graph and test for basic language model

--- tests/block_tests/test_seq_to_seq.py
+++ tests/block_tests/test_seq_to_seq.py
@@ -35,6 +35,11 @@ class TestSimpleBlocks(unittest.TestCase):
         filepath = self.setup_holder.filepath_handler.get_test_block_path(filename)
         self.setup_holder.block_loader.load(filepath)
 
+
+        data_filepath = self.setup_holder.filepath_handler.get_test_block_path(
+            "conll_reader_tests")
+        self.setup_holder.variable_repository.set_variable_value("data_folder", data_filepath)
+
         component_spec = CreationComponentSpecifications()
         component_spec.name = "cell"
         target_c = self.setup_holder.component_repository.get(component_spec)[0]
@@ -54,3 +59,25 @@ class TestSimpleBlocks(unittest.TestCase):
             for j in range(len(should_be[i])):
                 for k in range(len(should_be[i][j])):
                     self.assertEqual(should_be[i][j][k], output[i][j][k])
+
+
+    def testBasicLanguageModel(self):
+        filename = "seq_to_seq_tests/basic_language_model.xml"
+
+        filepath = self.setup_holder.filepath_handler.get_test_block_path(filename)
+        self.setup_holder.block_loader.load(filepath)
+
+        data_filepath = self.setup_holder.filepath_handler.get_test_block_path(
+            "conll_reader_tests")
+        self.setup_holder.variable_repository.set_variable_value("data_folder", data_filepath)
+
+        component_spec = CreationComponentSpecifications()
+        c = self.setup_holder.component_repository.get(component_spec)[0]
+        graph = c.get_graph()
+        ml_helper = self.setup_holder.ml_helper_factory.build_ml_helper_from_graph(graph)
+
+        ml_helper.train()
+        loss = ml_helper.validate()
+
+        print(loss)
+        self.assertTrue(False)

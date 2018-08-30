3496f2a68cd721191b7efbc36a7d463a9ad020f2
Schlichtkrull <micschli@8c85903f7e66.ant.amazon.com>
2018-08-30 17:22:06 +0200

    fixed test

--- tests/controller_tests/test_component_type_loader.py
+++ tests/controller_tests/test_component_type_loader.py
@@ -4,46 +4,44 @@ from Mindblocks.controller.component_type_loader.component_type_loader import Co
 from Mindblocks.helpers.files.FilepathHandler import FilepathHandler
 from Mindblocks.repository.component_type_repository.component_type_repository import ComponentTypeRepository
 from Mindblocks.repository.identifier_repository.identifier_repository import IdentifierRepository
+from tests.setup_holder import SetupHolder
 
 
 class TestComponentTypeLoader(unittest.TestCase):
 
     def setUp(self):
-        self.identifier_repository = IdentifierRepository()
-        self.type_repository = ComponentTypeRepository(self.identifier_repository)
-        self.filepath_handler = FilepathHandler()
-        self.component_type_loader = ComponentTypeLoader(self.filepath_handler, self.type_repository)
+        self.setup_holder = SetupHolder(load_default_types=False)
 
     def testLoadsSingleComponentType(self):
         filename = "arithmetics/add.py"
-        filepath = self.filepath_handler.get_default_component_type_file(filename)
-        self.component_type_loader.load_file(filepath)
+        filepath = self.setup_holder.filepath_handler.get_default_component_type_file(filename)
+        self.setup_holder.component_type_loader.load_file(filepath)
 
-        self.assertEqual(1, self.type_repository.count())
+        self.assertEqual(1, self.setup_holder.type_repository.count())
 
-        c_type = list(self.type_repository.elements.values())[0]
+        c_type = list(self.setup_holder.type_repository.elements.values())[0]
 
         self.assertEqual("Add", c_type.name)
 
     def testLoadsSingleComponentTypeWithCorrectSockets(self):
         filename = "arithmetics/add.py"
-        filepath = self.filepath_handler.get_default_component_type_file(filename)
-        self.component_type_loader.load_file(filepath)
+        filepath = self.setup_holder.filepath_handler.get_default_component_type_file(filename)
+        self.setup_holder.component_type_loader.load_file(filepath)
 
-        self.assertEqual(1, self.type_repository.count())
+        self.assertEqual(1, self.setup_holder.type_repository.count())
 
-        c_type = list(self.type_repository.elements.values())[0]
+        c_type = list(self.setup_holder.type_repository.elements.values())[0]
 
         self.assertEqual(["left", "right"], c_type.in_sockets)
         self.assertEqual(["output"], c_type.out_sockets)
 
     def testLoadsDirectory(self):
-        filepath = self.filepath_handler.get_default_component_type_folder()
-        self.component_type_loader.load_folder(filepath)
+        filepath = self.setup_holder.filepath_handler.get_default_component_type_folder()
+        self.setup_holder.component_type_loader.load_folder(filepath)
 
-        self.assertLess(0, self.type_repository.count())
+        self.assertLess(0, self.setup_holder.type_repository.count())
 
     def testLoadsDefaultDirectory(self):
-        self.component_type_loader.load_default_folder()
+        self.setup_holder.component_type_loader.load_default_folder()
 
-        self.assertLess(0, self.type_repository.count())
+        self.assertLess(0, self.setup_holder.type_repository.count())
